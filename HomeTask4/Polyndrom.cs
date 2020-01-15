4a677c53d772c44058d23bc6df97be748c8601ae
Kateryna Nesterova <Kateryna.Nesterova@teaminternational.com>
2020-01-15 19:34:58 +0200

    Add HomeTask

---
+++ HomeTask4/Polyndrom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask4
+{
+    public class Polyndrom
+    {
+        public static bool isPolyndrom(String input) {
+         
+                for (int i = 0; i < input.Length / 2; i++)
+                {
+                    if (input[i] != input[input.Length - 1 - i])
+                    {
+                        return false;
+                    }
+                }
+           
+            return true;
+        }
+    }
+}
---
+++ HomeTask4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application has started.");
+            ConsoleKeyInfo ExitCriteria;
+
+            for (int i = 0; i < 20; i++){
+                Console.WriteLine("Please input something...");
+
+                String Input = Console.ReadLine();
+
+                if (Input != null && Input != "")
+                {
+                    if (Polyndrom.isPolyndrom(Input))
+                    {
+                        Console.WriteLine("Your input is a polyndrom.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your input is not a polyndrom.");
+                    }
+                }
+                else {
+                    Console.WriteLine("Empty input.");
+                }
+                    
+
+                Console.WriteLine("Do you want to try again? Press 'N' to Exit or any other key to Continue.");
+
+                ExitCriteria = Console.ReadKey();
+                if (ExitCriteria.KeyChar == 'N' || ExitCriteria.KeyChar == 'n')
+                    break;
+            }
+
+            Console.WriteLine("Application has ended.");
+        }
+    }
+}
